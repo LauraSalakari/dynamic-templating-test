9bd732fc0ccd191ae2dd144157d832f838fe966d
Laura Salakari <salakari.laura@gmail.com>
2020-10-07 12:09:48 +0200

    activity completed

--- server.js
+++ server.js
@@ -10,14 +10,28 @@ app.set('views', __dirname + '/views');
 
 app.use(express.static('public'));
 
+let {students} = require("./exam-info");
+
 // 1: in the home,list all the students who took the exam (list all the students)
 
-// ... Your code here
+app.get("/", (req, res) => {
+  res.render("full-list.hbs", {students});
+});
 
 // 2: in the '/results' list all the students who passed the test and their score.
 // Also, students should be in descending order based on their score.
 
-// ... Your code here
+app.get("/results", (req, res) => {
+  students = students.filter((elem) => {
+    return elem.hasPassed;
+  }).sort((a,b) => {
+    if(a.score > b.score) return -1;
+    else if(a.score < b.score) return 1;
+    else return 0;
+  });
+
+  res.render("results.hbs", {students});
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`App running on ${process.env.PORT}.`)
